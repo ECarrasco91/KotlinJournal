97e253210ce4b27137957bd380a3a15ae2e9eb31
ECarrasco91 <ezequielcarrasco91@yahoo.com>
2022-05-17 01:23:26 -0400

    modified activity

---
+++ app/src/main/java/com/ezequielc/kotlinjournal/JournalApplication.kt
@@ -0,0 +1,7 @@
+package com.ezequielc.kotlinjournal
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JournalApplication : Application ()
--- app/src/main/java/com/ezequielc/kotlinjournal/ui/JournalActivity.kt
+++ app/src/main/java/com/ezequielc/kotlinjournal/ui/JournalActivity.kt
@@ -2,11 +2,78 @@ package com.ezequielc.kotlinjournal.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.get
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.ezequielc.kotlinjournal.R
+import com.ezequielc.kotlinjournal.databinding.ActivityJournalBinding
+import com.ezequielc.kotlinjournal.ui.journal.JournalFragmentDirections
+import dagger.hilt.android.AndroidEntryPoint
 
-class JournalActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class JournalActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityJournalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_journal)
+
+        binding = ActivityJournalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener(this)
+
+        binding.fab.setOnClickListener {
+            val direction = JournalFragmentDirections.actionJournalFragmentToAddEditJournalFragment()
+            navController.navigate(direction)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_delete_all -> {
+                // TODO: Implement DeleteAllDialogFragment()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?,
+    ) {
+        val journalDestination = controller.graph[R.id.JournalFragment]
+        val addEditJournalDestination = controller.graph[R.id.AddEditJournalFragment]
+        when (destination) {
+            journalDestination -> binding.fab.visibility = View.VISIBLE
+            addEditJournalDestination -> binding.fab.visibility = View.INVISIBLE
+        }
     }
 }
